03e0bb5250914b0e357ce22d934477c395cda54e
Sudhanshu Bhatt <101445771+Sudhanshubhatt12@users.noreply.github.com>
2024-12-22 08:59:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,45 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+   arr
+    .map((employee) => {
+      if (employee.profession === "developer") {
+        console.log(employee);
+      }
+    });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+   arr.forEach((employee) => {
+    if (employee.profession === "developer") {
+      console.log(employee);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+    const newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newEmployee);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+   arr = employees.filter((employee) => employee.profession !== "admin");
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const newEmployees = [
+    { id: 5, name: "lisa", age: "22", profession: "designer" },
+    { id: 6, name: "mike", age: "23", profession: "developer" },
+    { id: 7, name: "nancy", age: "21", profession: "manager" },
+  ];
+  const concatenatedArray = arr.concat(newEmployees);
+  console.log(concatenatedArray);
 }
+
+
